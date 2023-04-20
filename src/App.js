9ad6d21c101bf1fb300be5b089a366a5e4d27f50
NSINGHRAJPUT <nsinghrajput30@gmail.com>
2023-04-20 21:11:46 +0530

    usingAsyncFunction

--- src/App.js
+++ src/App.js
@@ -6,21 +6,33 @@ import './App.css';
 function App() {
   const [movies,setMovies] = useState([]);
 
-  function fetchMovies (){
-    fetch('https://swapi.dev/api/films').then((respone)=>{
-      return respone.json();
-    })
-    .then((data)=>{
-      const transformedMovies = data.results.map((moviesData)=> {
-        return {
-          id:moviesData.episode_id,
-          title:moviesData.title,
-          openingText:moviesData.opening_crawl,
-          releaseDate:moviesData.release_date
-        };
-      })
-      setMovies(transformedMovies)
-    })
+  async function fetchMovies (){
+    const response = await fetch('https://swapi.dev/api/films')
+    // .then((respone)=>{
+    //   return respone.json();
+    // })
+    const data = await response.json();
+    console.log(data)
+    const transformedMovies = data.results.map((moviesData)=> {
+          return {
+            id:moviesData.episode_id,
+            title:moviesData.title,
+            openingText:moviesData.opening_crawl,
+            releaseDate:moviesData.release_date
+          };
+        })
+        setMovies(transformedMovies)
+    // .then((data)=>{
+    //   const transformedMovies = data.results.map((moviesData)=> {
+    //     return {
+    //       id:moviesData.episode_id,
+    //       title:moviesData.title,
+    //       openingText:moviesData.opening_crawl,
+    //       releaseDate:moviesData.release_date
+    //     };
+    //   })
+      // setMovies(data)
+    // })
   }
 
   return (
